1883c60ad6e9ccb1ddf70479ba1db9dab4de9887
pufferffish <github@bandersnatch.anonaddy.com>
2023-11-19 01:00:35 +0000

    init

---
+++ src/client/java/octi/wanparty/WANPartyClient.java
@@ -0,0 +1,10 @@
+package octi.wanparty;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WANPartyClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
